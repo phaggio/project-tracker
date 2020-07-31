b07f2670773e34a625321f113da668f9bfa571ac
phaggio <richardchacha@hotmail.com>
2020-07-30 21:01:08 -0700

    collapse navbar on click

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import $ from 'jquery';
+
+const consoleThis = (e: { target: Object; }) => {
+  console.log(e.target);
+}
 
 
 const NavBar = () => {
+  const toggle = () => {
+    $('.navbar-collapse').collapse('hide');
+  }
+
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-light">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbar" aria-controls="navbar"
+        aria-expanded="true" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <div className="collapse navbar-collapse" id="navbar">
         <a className="navbar-brand" href="#">Project Tracker</a>
         <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
           <li className="nav-item">
-            <NavLink exact className="nav-link" to="/">Home</NavLink>
+            <NavLink exact className="nav-link" to="/" onClick={toggle}>Home</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink exact className="nav-link" to="/boards">Boards</NavLink>
+            <NavLink exact className="nav-link" to="/boards" onClick={toggle}>Boards</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink exact className="nav-link" to="/settings">Settings</NavLink>
+            <NavLink exact className="nav-link" to="/settings" onClick={toggle}>Settings</NavLink>
           </li>
           {/* <li className="nav-item">
             <a className="nav-link disabled" href="#" tabIndex={1} aria-disabled="true">Disabled</a>
