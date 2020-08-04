7cba8b34988af73bdbd621b29945b4c964ede205
phaggio <richardchacha@hotmail.com>
2020-08-03 20:44:18 -0700

    icons

--- client/src/pages/Settings/index.tsx
+++ client/src/pages/Settings/index.tsx
@@ -4,13 +4,20 @@ import React from 'react';
 const Settings = () => {
 
   return (
-    <div>this is Settings page
+    <div className="container">this is Settings page
 
 
 
       <div className="container border border-warning">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur quo, asperiores repudiandae hic tenetur placeat quisquam iure quam unde eveniet illum aperiam, ex totam! Delectus eveniet a consequuntur inventore?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Suscipit pariatur quo, asperiores repudiandae hic tenetur placeat quisquam iure quam unde eveniet illum aperiam, ex totam! Delectus eveniet a consequuntur inventore?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe corporis unde ullam laborum sit facilis quas eveniet officiis, magni in sequi repudiandae ex ipsa tempora dignissimos animi dolor tempore fugit.
       </div>
+
+      <small>
+        Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik </a>
+      from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </small>
     </div>
   )
 }
